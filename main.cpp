7ada7e3d8ea4b73184380d158833a493c30a70f5
Alena Kozlova <alena.kozlova2004@mail.ru>
2024-04-07 21:22:21 +0300

    added functions to finding combinations

--- main.cpp
+++ main.cpp
@@ -6,9 +6,12 @@ using namespace std;
 typedef vector<vector<int>> Matrix;
 
 void printMatrix(Matrix&);
+Matrix copy(Matrix&);
 Matrix fillRandomMatrix(int, int);
 void leadByRows(Matrix&);
 void leadByColumns(Matrix&);
+pair<Matrix, Matrix> findLargestMatch(Matrix&);
+void fillDigraph(Matrix&, Matrix&);
 
 auto findMax{ [](Matrix& matrix) {int max = matrix[0][0];
 for (int i = 0; i < matrix.size(); i++)
@@ -49,13 +52,28 @@ for (int i = 0; i < matrix.size(); i++)
 }
 } };
 
+auto countMatches{ [](Matrix& matrix) {int count = 0;
+for (int i = 0; i < matrix.size(); i++)
+{
+	for (int j = 0; j < matrix[i].size(); j++)
+	{
+		if (matrix[i][j] == 1)
+		{
+			count++;
+		}
+	}
+}
+
+	return count;
+} };
+
 
 void main() {
-	Matrix matrix = fillRandomMatrix(9, 20);
+	Matrix matrix = fillRandomMatrix(9, 10);
 	printMatrix(matrix);
 
 	int max = findMax(matrix);
-	cout << max << endl;
+	cout << max << endl << endl;
 
 	decrement(matrix, max);
 	printMatrix(matrix);
@@ -68,6 +86,28 @@ void main() {
 
 	invert(matrix);
 	printMatrix(matrix);
+
+	Matrix copyInvertMatrix = copy(matrix);
+
+	pair<Matrix, Matrix> matrices = findLargestMatch(matrix);
+
+	Matrix matchMatrix = matrices.first;
+	printMatrix(matchMatrix);
+
+	Matrix digraph = matrices.second;
+	fillDigraph(digraph, copyInvertMatrix);
+	printMatrix(digraph);
+
+	int count = countMatches(matchMatrix);
+
+	if (count == matrix.size())
+	{
+		cout << "" << endl;
+	}
+	else
+	{
+
+	}
 }
 
 
@@ -127,4 +167,62 @@ void leadByColumns(Matrix& matrix)
 			matrix[j][i] -= min;
 		}
 	}
+}
+
+pair<Matrix, Matrix> findLargestMatch(Matrix& matrix)
+{
+	Matrix matchMatrix(matrix.size(), vector<int>(matrix[0].size(), 0));
+	Matrix digraph(matrix.size(), vector<int>(matrix[0].size(), 0));
+
+	for (int i = 0; i < matrix.size(); i++)
+	{
+		for (int j = 0; j < matrix[i].size(); j++)
+		{
+			if (matrix[i][j] == 1)
+			{
+				matchMatrix[i][j] = 1;
+				digraph[i][j] = 1;
+
+				for (int k = i; k < matrix.size(); k++)
+				{
+					if (matrix[k][j] == 1)
+					{
+						matrix[k][j] = 0;
+					}
+				}
+
+				break;
+			}
+		}
+	}
+
+	return make_pair(matchMatrix, digraph);
+}
+
+void fillDigraph(Matrix& digraph, Matrix& invert)
+{
+	for (int i = 0; i < digraph.size(); i++)
+	{
+		for (int j = 0; j < digraph[i].size(); j++)
+		{
+			if (invert[i][j] == 1 && digraph[i][j] == 0)
+			{
+				digraph[i][j] = -1;
+			}
+		}
+	}
+}
+
+Matrix copy(Matrix& matrix)
+{
+	Matrix copyMatrix(matrix.size(), vector<int>(matrix[0].size(), 0));
+	for (int i = 0; i < matrix.size(); i++)
+	{
+		for (int j = 0; j < matrix[i].size(); j++)
+		{
+			copyMatrix[i][j] = matrix[i][j];
+		}
+	}
+
+	return copyMatrix;
 }
