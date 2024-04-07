b34376d982173e2155540544ccfd2b5596dbb9d1
Alena Kozlova <alena.kozlova2004@mail.ru>
2024-04-07 19:32:36 +0300

    added functions for working with matrix

--- main.cpp
+++ main.cpp
@@ -1,7 +1,58 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-void main() {
+typedef vector<vector<int>> Matrix;
 
+void printMatrix(Matrix&);
+Matrix fillRandomMatrix(int, int);
+
+auto findMax{ [](Matrix& matrix) {int max = matrix[0][0];
+	for (int i = 0; i < matrix.size(); i++)
+	for (int j = 0; j < matrix[i].size(); j++)
+	{
+		max = (matrix[i][j] > max) ? matrix[i][j] : max;
+	}
+	return max;
+} };
+
+auto decrement{ []() {
+
+}
+};
+
+void main() {
+	Matrix matrix = fillRandomMatrix(9, 20);
+	printMatrix(matrix);
+
+	int max = findMax(matrix);
+	cout << max;
+}
+
+void printMatrix(Matrix& matrix) {
+	for (int i = 0; i < matrix.size(); i++)
+	{
+		for (int j = 0; j < matrix[i].size(); j++)
+		{
+			cout << matrix[i][j] << " ";
+		}
+		cout << endl;
+	}
+}
+
+Matrix fillRandomMatrix(int counRow, int countColumn) {
+	Matrix matrix(counRow, vector<int>(countColumn, 0));
+
+	for (int i = 0; i < counRow; i++)
+	{
+		vector<int> row(countColumn, 0);
+		for (int j = 0; j < countColumn; j++)
+		{
+			row[j] = rand() % 10;
+		}
+		matrix[i] = row;;
+	}
+
+	return matrix;
 }
